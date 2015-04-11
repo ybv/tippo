e9f92e85249ad55d3cffb979200dd1e91238c728
ybv <ybv.kris@gmail.com>
2015-04-10 23:28:36 -0700

    Initial Commit

---
+++ Tippo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tippo
+//
+//  Created by Vaibhav Krishna on 4/10/15.
+//  Copyright (c) 2015 Vaibhav Krishna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
