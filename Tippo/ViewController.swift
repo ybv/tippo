d44c74d3607851e667cfe9a9cb23d3b5d5eee90b
ybv <ybv.kris@gmail.com>
2015-04-13 21:09:00 -0700

    initial checkin

--- Tippo/ViewController.swift
+++ Tippo/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billAmountField: UITextField!
+    @IBOutlet weak var tipSegment: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+  
+    @IBAction func onEdit(sender: AnyObject) {
+        let tipArray = [0.10,0.20,0.30]
+        var billAmount = (billAmountField.text as NSString).doubleValue
+        var tipAmount = tipArray[tipSegment.selectedSegmentIndex]
+        var tip = billAmount * tipAmount
+        var total = billAmount + tip
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
